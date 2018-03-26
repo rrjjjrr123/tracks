6bc7d7e67c5f665bf5e03119eb187627f3090af0
Ronak <ronak.jain2116@gmail.com>
2018-03-26 13:27:06 +0530

    Major Project

---
+++ Downloads/tracks-2.3.0/app/assets/javascripts/application.js
@@ -0,0 +1,33 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+
+// Stuff in app/assets
+//= require tracks
+//= require tracks_pages
+//= require disable_fx_in_test
+
+// Stuff in vendor/assets
+//= require jquery-ui-1.10.0.custom.min
+//= require jquery.ui.touch-punch.min
+//= require jquery.blockUI
+//= require jquery.cookie
+//= require jquery.form
+//= require jquery.jeditable.mini
+//= require jquery.simulate.drag-sortable
+//= require jquery.truncator
+//= require niftycube
+//= require superfish
+//= require supersubs
+//= require swf_fu
